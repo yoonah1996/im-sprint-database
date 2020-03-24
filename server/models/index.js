ca5896702df2aa52eb5ff37d4c3489443c92de3b
yoonah1996 <yoonah1996@gmail.com>
2020-03-24 17:44:11 +0900

    orm update complete password

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ var Sequelize = require("sequelize");
 var db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DATABASE_SPRINT_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: "mysql"
